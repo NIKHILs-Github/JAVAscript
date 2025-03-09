8ab40a6a4e629045749f12394e946d4e2bd34b71
Nikhil_negi <negi66534@gmail.com>
2025-03-09 14:10:42 +0000

    container config done

---
+++ basics/Variables.js
@@ -0,0 +1,19 @@
+// Constants and variables
+ const account_Id=20034
+ let account_Mail="negi66534@gmail.com"
+ var password="123400"
+ city="Delhi"
+ let state;
+
+ //account_Id=20 // not allowed const variable cannot be changed
+
+ //Let is used for scope 
+
+ //Prefer not to use var bcz of issue in block scope and functional scope
+
+ account_Mail="h@mail.com"
+ password="nikhil2004"
+ city="Dehradun"
+
+ console.log(account_Id);
+ console.table([account_Id,account_Mail,password,city,state]);
