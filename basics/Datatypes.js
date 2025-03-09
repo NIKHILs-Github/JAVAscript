f8504231f17c028c0cf607ba63b0d46a6c2b7ca2
Nikhil_negi <negi66534@gmail.com>
2025-03-09 14:33:00 +0000

    dataypes

---
+++ basics/Datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+//Treat JS as newer version
+
+//alert("hello")// Works only on browser ot on node js
+
+console.log(3+3);
+
+console.log("hello");
+
+let name="nikhil" //string datatype
+let age=30        //Number datatype
+let login=true    //Boolean Datatype
+                  //null standalone it is also object itself
+                  //undefined 
+                  //symbol is unique
+                  //object
+console.log(typeof);
+
